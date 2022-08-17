c721e9d297a74fcf18c7203ef2772c469fc840cb
Venkata Naga Raju Ramu <rvnr1990@outlook.com>
2022-08-17 15:16:38 +0530

    Script sql

---
+++ CodeTest170822/SQLScripts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeTest170822
+{
+    public class SQLScripts
+    {
+
+        //Create Table Customer costomerID varchar primarykey,name varchar;
+
+        //Create Table Product ProductName varchar,Price decimal,Description varchar;
+    }
+}
